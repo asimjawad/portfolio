8b3ee2afdb9f34e01cf9bca90acd7adeafb6dc7c
Muhammad Asim Jawad <asimjawad2723@gmail.com>
2023-08-10 23:26:40 +0500

    first commit

--- lib/core/models/project.dart
+++ lib/core/models/project.dart
@@ -2,19 +2,22 @@ class Project {
   final String? githubLink;
   final String? websiteLink;
   final String? playstoreLink;
+  final String? appStoreLink;
   final String title;
   final String description;
   final List<String> tools;
   final String id;
   final List<String> type;
 
-  Project(
-      {this.githubLink,
-      this.websiteLink,
-      this.playstoreLink,
-      required this.id,
-      required this.type,
-      required this.title,
-      required this.description,
-      required this.tools});
+  Project({
+    this.githubLink,
+    this.websiteLink,
+    this.playstoreLink,
+    this.appStoreLink,
+    required this.id,
+    required this.type,
+    required this.title,
+    required this.description,
+    required this.tools,
+  });
 }
